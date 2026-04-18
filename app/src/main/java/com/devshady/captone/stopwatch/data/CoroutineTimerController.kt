962a40ea10e205af07ce9c21cf22bbd954f5ebb4
Amit Kumar <itsdevshady@gmail.com>
2026-04-18 19:22:52 +0530

    [Perf Fix] Stop timer controller if UI is not visible
    1. Earlier the timer controller was still running its loop even if UI was not visible, consuming unnecessary cpu cycles.
    2. Create a cold flow using duration and update interval.
    3. Convert it into a hot flow with WhileSubscribed as 5sec.
    4. Since timer runs only when start button is triggered by user, use another flow until then which always emits TimerState.Idle.
    5. Switch to TimerState.Running flow with start command.
    6. Now the timer controller will stop running after 5 sec if there are no active listeners.

--- app/src/main/java/com/devshady/captone/stopwatch/data/CoroutineTimerController.kt
+++ app/src/main/java/com/devshady/captone/stopwatch/data/CoroutineTimerController.kt
@@ -4,20 +4,44 @@ import android.os.SystemClock
 import com.devshady.captone.stopwatch.domain.TimerController
 import com.devshady.captone.stopwatch.domain.TimerState
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.stateIn
 
-internal class CoroutineTimerController private constructor(val scope: CoroutineScope) :
+internal class CoroutineTimerController private constructor(applicationScope: CoroutineScope) :
     TimerController {
 
-    private val timeFlowMutable = MutableStateFlow<TimerState>(TimerState.Idle)
-    override val timerState = timeFlowMutable.asStateFlow()
-    private var timerJob: Job? = null
+    private val timerCommand = MutableStateFlow<TimerCommand>(TimerCommand.Stop)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val timerState: StateFlow<TimerState> = timerCommand.flatMapLatest { command ->
+        when (command) {
+            is TimerCommand.Stop -> {
+                flowOf(TimerState.Idle)
+            }
+
+            is TimerCommand.Start -> {
+                getTimerFlow(command.duration, command.updateInterval)
+                    .onCompletion { cause ->
+                        if (cause == null) {
+                            timerCommand.value = TimerCommand.Stop
+                        }
+                    }
+            }
+        }
+    }.stateIn(
+        scope = applicationScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = TimerState.Idle
+    )
 
     companion object {
         private var instance: TimerController? = null
@@ -28,32 +52,33 @@ internal class CoroutineTimerController private constructor(val scope: Coroutine
         }
     }
 
-    override fun start(totalTimeInMillis: Long, updateInterval: Long) {
+    fun getTimerFlow(totalTimeInMillis: Long, updateInterval: Long): Flow<TimerState> = flow {
         val endClockTime = SystemClock.elapsedRealtime() + totalTimeInMillis
-        timerJob?.cancel()
-        timerJob = scope.launch(Dispatchers.Default) {
-            // start the countdown timer off main thread
-            while (isActive && SystemClock.elapsedRealtime() < endClockTime) {
-                timeFlowMutable.value =
-                    TimerState.Running(endClockTime - SystemClock.elapsedRealtime())
-                delay(updateInterval)
-            }
-
-            if (isActive) {
-                timeFlowMutable.value = TimerState.Finished
-            }
+        // start the countdown timer off main thread
+        while (SystemClock.elapsedRealtime() < endClockTime) {
+            emit(TimerState.Running(endClockTime - SystemClock.elapsedRealtime()))
+            delay(updateInterval)
         }
     }
 
+    override fun start(totalTimeInMillis: Long, updateInterval: Long) {
+        timerCommand.value = TimerCommand.Start(totalTimeInMillis, updateInterval)
+    }
+
     override fun pause() {
-        timerJob?.cancel()
+        //pause the timer
     }
 
     override fun stop() {
-        timerJob?.cancel()
+        timerCommand.value = TimerCommand.Stop
     }
 
-    override fun reset() {
-        TODO("Not yet implemented")
+    override fun resume() {
+        TODO("resume paused timer")
+    }
+
+    sealed class TimerCommand {
+        data class Start(val duration: Long, var updateInterval: Long) : TimerCommand()
+        object Stop : TimerCommand()
     }
 }
